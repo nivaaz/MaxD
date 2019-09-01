749aabe934465e79345ecd971c3dd0145497d719
nivaaz <nksehmbhi@gmail.com>
2019-09-01 13:33:38 +1000

    commected out backend

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
-// import entire SDK
-import AWS from 'aws-sdk';
-// import AWS object without services
-import AWS from 'aws-sdk/global';
-// import individual service
-import S3 from 'aws-sdk/clients/s3';
+// // import entire SDK
+// import AWS from 'aws-sdk';
+// // import AWS object without services
+// import AWS_ from 'aws-sdk/global';
+// // import individual service
+// import S3 from 'aws-sdk/clients/s3';
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-import express from "express";
-import path from "path";
-const app = express();
+// import express from "express";
+// import path from "path";
+// const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
-const router = express.Router();
+// const router = express.Router();
 
-const staticFiles = express.static(path.join(__dirname, "../../frontend/build"));
-app.use(staticFiles);
+// const staticFiles = express.static(path.join(__dirname, "../../frontend/build"));
+// app.use(staticFiles);
 
-// Example API call - To remove
-router.get("/api/hello", (req, res) =>
-  res.json({
-    name: "sukara"
-  })
-);
+// // Example API call - To remove
+// router.get("/api/hello", (req, res) =>
+//   res.json({
+//     name: "sukara"
+//   })
+// );
 
-app.use(router);
+// app.use(router);
 
-// any routes not picked up by the server api will be handled by the react router
-app.use("/*", staticFiles);
+// // any routes not picked up by the server api will be handled by the react router
+// app.use("/*", staticFiles);
 
-app.set("port", process.env.PORT || 3001);
-app.listen(app.get("port"), () => {
-  console.log(`Listening on ${app.get("port")}`);
-});
+// app.set("port", process.env.PORT || 3001);
+// app.listen(app.get("port"), () => {
+//   console.log(`Listening on ${app.get("port")}`);
+// });
